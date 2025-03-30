cb381991c8ea880da8800cfe8d48281da3c061a0
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2025-03-30 18:30:46 +0200

    Done

--- presentation/presentation.py
+++ presentation/presentation.py
@@ -6,17 +6,31 @@ config.background_color = "#f5f2eb"
 
 class FOCS(slide.Slide):
     def construct(self):
-        title = Text(
-            "IEEE Symposium on Foundations of Computer Science", color=BLACK
-        ).scale(1.2)
+        # TitleSlide
+        title1 = Tex(r"\textbf{IEEE Symposium on}", color=BLACK).scale(1.2)
+        title2 = Tex(r"\textbf{Foundations of Computer Science}", color=BLACK).scale(
+            1.2
+        )
+
+        title_group = (
+            VGroup(title1, title2)
+            .arrange(DOWN, aligned_edge=LEFT, buff=0.2)
+            .to_edge(LEFT, buff=1)
+            .shift(UP * 0.7)
+        )
+
         author = (
-            Text(
+            Tex(
                 "Matej B., Nik Č., Klemen K., Tomaž J. L., Bor P., Marko R., Pia S., Vanja S.",
                 color=BLACK,
             )
             .scale(0.7)
-            .shift(DOWN)
+            .shift(DOWN * 2)
+            .to_edge(LEFT, buff=1)
         )
-        self.play(Write(title))
-        self.play(Write(author))
+        self.play(Write(title_group))
+
+        self.play(Write(author, run_time=0.8))
         self.next_slide()
+
+        # First slide
